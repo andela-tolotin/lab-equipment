b939311417d8a4a7131750a354c7c31e76c98cba
Temitope Olotin <temitope.olotin@andela.com>
2017-01-15 19:00:03 +0100

    Add names to the basic form

--- resources/views/admin/basic_info/edit_basic_info.blade.php
+++ resources/views/admin/basic_info/edit_basic_info.blade.php
@@ -8,19 +8,19 @@
     <div class="form-group">
         <label for="email" class="col-sm-2 control-label">Email</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="email" placeholder="Email" value="{{ Auth::user()->email }}" readonly="readonly">
+            <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="{{ Auth::user()->email }}" readonly="readonly">
         </div>
     </div>
     <div class="form-group">
         <label for="phone" class="col-sm-2 control-label">Phone</label>
         <div class="col-sm-10">
-            <input type="email" class="form-control" id="phone" placeholder="Phone" value="{{ Auth::user()->phone }}">
+            <input type="email" class="form-control" id="phone" name="phone" placeholder="Phone" value="{{ Auth::user()->phone }}">
         </div>
     </div>
     <div class="form-group">
         <label for="office" class="col-sm-2 control-label">Office</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="office" placeholder="Office">
+            <input type="text" class="form-control" id="office" name="office" placeholder="Office">
         </div>
     </div>
     <div class="form-group">
@@ -29,19 +29,19 @@
     <div class="form-group">
         <label for="c_password" class="col-sm-2 control-label">Enter Current Password</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="c_password" placeholder="Enter Current Password">
+            <input type="text" class="form-control" id="c_password" name="c_password" placeholder="Enter Current Password">
         </div>
     </div>
     <div class="form-group">
         <label for="new_password" class="col-sm-2 control-label">Enter New Password</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" id="new_password" placeholder="Enter New Password">
+            <input type="password" class="form-control" id="new_password" name= "new_password" placeholder="Enter New Password">
         </div>
     </div>
     <div class="form-group">
         <label for="com_password" class="col-sm-2 control-label">Confirm New Password</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="com_password" placeholder="Confirm New Password">
+            <input type="text" class="form-control" id="com_password" name="com_password" placeholder="Confirm New Password">
         </div>
     </div>
     <div class="form-group">
