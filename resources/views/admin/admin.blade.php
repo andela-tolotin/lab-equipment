4fafc2cd013b685c499ae2b9f74a61d73abadb43
Temitope Olotin <temitope.olotin@andela.com>
2017-01-21 22:36:02 +0100

    get user assigned to lab

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -170,7 +170,8 @@
                     <tbody>
                         <tr>
                             <td><strong>{{ Auth::user()-> name }}</strong></td>
-                            <td><strong>Lab: {{ Auth::user()->labUser[0]->user->name }}</strong></td>
+                            <td><strong>Lab: {{ 
+                                !is_null(@Auth::user()->labUser[0])? Auth::user()->labUser[0]->user->name: 'Nill' }}</strong></td>
                             <td><a href="#" title="{{ Auth::user()-> name}}">See my page</a></td>
                         </tr>
                     </tbody>
