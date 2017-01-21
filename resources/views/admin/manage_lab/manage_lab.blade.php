91add25d452e5b821640eff8377448257b231238
Temitope Olotin <temitope.olotin@andela.com>
2017-01-21 12:57:25 +0100

    Seperate creating user with a lab

--- resources/views/admin/manage_lab/manage_lab.blade.php
+++ resources/views/admin/manage_lab/manage_lab.blade.php
@@ -13,6 +13,17 @@
         </div>
     </div>
     <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-large btn-default" id="save-lab">Save</button>
+        </div>
+    </div>
+</form>
+<hr>
+<h6>Assign user to lab</h6>
+<hr>
+<form class="form-horizontal" id="assign_user_to_lab">
+    <input type="hidden" name="_token" id="_token" class="form-control" value="{{ csrf_token() }}">
+    <div class="form-group">
         <label for="assign_user" class="col-sm-2 control-label">Assign User</label>
         <div class="col-sm-10">
             <select name="assign_user" id="assign_user" class="form-control" required="required">
@@ -20,6 +31,19 @@
                 @if ($users->count() > 0)
                 @foreach($users as $user)
                 <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach
+                @endif
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="assign_user" class="col-sm-2 control-label">Assign User</label>
+        <div class="col-sm-10">
+            <select name="assign_user" id="assign_user" class="form-control" required="required">
+                <option value="">Choose Lab</option>
+                @if ($labs->count() > 0)
+                @foreach($labs as $lab)
+                <option value="{{ $lab->id }}">{{ $lab->title }}</option>
                 @endforeach
                 @endif
             </select>
