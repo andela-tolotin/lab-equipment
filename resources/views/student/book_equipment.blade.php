34a9f4a0457f6bd603e5e09326751bd2a0fe7900
Temitope Olotin <temitope.olotin@andela.com>
2017-01-22 23:09:12 +0100

    Display 24hrs for day

--- resources/views/student/book_equipment.blade.php
+++ resources/views/student/book_equipment.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
+		<div class="col-md-10 col-md-offset-1">
 			{{-- @include('admin.manage_user_account.logout') --}}
 			<h5><a href="{{ route('my_profile') }}" class="pull-left"> << My page </a></h5><br>
 			<hr>
@@ -32,7 +32,7 @@
 			<hr>
 			{{-- <div class="container"> --}}
 				<div class="row">
-					<div class="col-md-5">
+					<div class="col-md-4">
 						<div class="form-group">
 							<div class='input-group date' id='datetimepicker1'>
 								<input type='text' class="form-control" />
@@ -52,13 +52,54 @@
 						<h5>Before 1hr of reservation</h5>
 						<hr>
 					</div>
-					<div class="col-md-7">
-					<h5>You select <span id="time"></span></h5>
+					<div class="col-md-8">
+						<h5>You select <span id="time"></span></h5>
 						<table class="table table-hover">
 							<tbody>
+								<?php $count = 0; ?>
+								<?php for ($i = 9; $i <= 24; $i++): ?>
 								<tr>
-									<td></td>
+									<td>{{ $i.":00" }}</td>
+									<td>
+										<div class="checkbox">
+											<label>
+												<input type="checkbox" value="">
+												:00 - 10
+											</label>
+										</div>
+									</td>
+									<td><div class="checkbox">
+										<label>
+											<input type="checkbox" value="">
+											:10 - 20
+										</label>
+									</div></td>
+									<td><div class="checkbox">
+										<label>
+											<input type="checkbox" value="">
+											:20 - 30
+										</label>
+									</div></td>
+									<td><div class="checkbox">
+										<label>
+											<input type="checkbox" value="">
+											:30 - 40
+										</label>
+									</div></td>
+									<td><div class="checkbox">
+										<label>
+											<input type="checkbox" value="">
+											:40 - 50
+										</label>
+									</div></td>
+									<td><div class="checkbox">
+										<label>
+											<input type="checkbox" value="">
+											:50 - 00
+										</label>
+									</div></td>
 								</tr>
+								<?php endfor ?>
 							</tbody>
 						</table>
 					</div>
